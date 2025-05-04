109313791f539d7e4b03fc21a3ee389524ddc029
Da-Scher <Dak.Scher@proton.me>
2025-05-03 22:18:47 -0700

    added struct ntb to ntb.h

---
+++ ntb.h
@@ -0,0 +1,21 @@
+#ifndef NTB_H
+#define NTB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/* typedef struct node_tree_buffer ntb
+ * Members
+ * uint8_t* buffer -- the character buffer of any potential size
+ * ntb* l -- the left child of this node
+ * ntb* r -- the right child of this node
+**/
+typedef struct node_tree_buffer {
+	uint8_t* buffer;
+	ntb* l;
+	ntb* r;
+} ntb;
+
+
+#endif
