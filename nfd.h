9eb2cb0c6ee93bed335e8114102f50c8463e0996
Da-Scher <Dak.Scher@proton.me>
2025-05-04 14:07:19 -0700

    :heavy_check_mark: Task: Add description for function

--- nfd.h
+++ nfd.h
@@ -16,11 +16,11 @@ typedef struct node_fd {
 	nfd* prev;
 } nfd
 
-/*
+/* nfd* new_file(int16_t) -- Create a new member of the nfd double-linked list
+ * Arguments:
+ * int16_t fd -- The file descriptor to be added to the list. 
  *
- *
- *
- *
+ * Returns the new member of the double-linked list.
 **/
 nfd* new_file(int16_t fd);
 
