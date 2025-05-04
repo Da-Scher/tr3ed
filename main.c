d42d9b82ee3391ca30b1e93a48898abf5810ab6e
Da-Scher <Dak.Scher@proton.me>
2025-05-04 15:07:44 -0700

    main.c init

---
+++ main.c
@@ -0,0 +1,11 @@
+/* Author: Dakota Schaeffer
+ * GH Handle: Da-Scher
+ * Date: 5/3/25
+ * Note: Lovingly crafted in ed
+**/
+#include <stdio.h>
+#include <stdint.h>
+
+int32_t main(uint32_t argc, uint8_t **argv) {
+	return 0;
+}
